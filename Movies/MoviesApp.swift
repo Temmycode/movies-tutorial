39d9fca280f80d0c41fc5b3cc5331deca24ab778
Akisanya Temiloluwa <tolutech2004@gmail.com>
2024-01-01 14:07:53 +0100

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Temiloluwa Akisanya on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
